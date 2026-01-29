383891f95aa108b7b60579bc2e3b262f48943278
riccardogiussani <riciriccio@hotmail.it>
2026-01-29 17:14:09 +0100

    Updated gitignore and vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite';
 
-// Get the repository name from environment variables or set it manually
-const REPO_NAME = 'three-js-app'; // Replace with your actual GitHub repository name
+const REPO_NAME = 'three-js-app';
 
 export default defineConfig({
-  // Sets the base public path for the project.
-  // This is crucial for GitHub Pages to correctly load assets (like /src/main.ts).
-  base: `/${REPO_NAME}/`,
+  base: `/${REPO_NAME}/`
 });
